1632489a360d15c305e615b7a890932d2eabdefb
@eapesteguia <ezequiel.apesteguia@gmail.com>
2024-10-09 21:48:29 -0300

    ping-pong

--- src/app.js
+++ src/app.js
@@ -15,6 +15,10 @@ const PORT = 8080;
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter);
 
+app.use("/ping", (req, res) => {
+  res.send("pong");
+});
+
 app.listen(PORT, () => {
   console.log(`Server escuchando en el puerto ${PORT}`);
 });
