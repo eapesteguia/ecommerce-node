99c53e03d28c43b5414d51e63fa61f183ac5748f
@eapesteguia <ezequiel.apesteguia@gmail.com>
2024-10-21 12:04:57 -0300

    Config para deploy en Glitch

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ import { Server } from "socket.io";
 
 // config express
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
